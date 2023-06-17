9652ac29d929340845482d895323f70744920c0c
av3r6un <av3rgun@yandex.com>
2023-06-18 02:38:50 +0300

    Adding logging protocol

--- start.py
+++ start.py
@@ -3,6 +3,10 @@ from bot import WaveBox
 from config import Config
 import asyncio
 import discord
+import logging
+
+
+handler = logging.FileHandler(filename='discord-error.log', encoding='utf-8', mode='w')
 
 
 intents = discord.Intents.default()
@@ -11,7 +15,7 @@ intents.message_content = True
 bot = commands.Bot(
 	command_prefix=commands.when_mentioned_or('-'),
 	description=Config.BOT_DESCR,
-	intents=intents
+	intents=intents,
 )
 
 
@@ -25,6 +29,8 @@ async def main():
 	async with bot:
 		await bot.add_cog(WaveBox(bot))
 		await bot.start(Config.TOKEN)
+
+discord.utils.setup_logging(handler=handler, level=logging.ERROR)
 
 
 asyncio.run(main())
